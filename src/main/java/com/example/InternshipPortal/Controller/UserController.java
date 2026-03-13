92336088b49404a9ef1e04efc734f85eadf73205
Valeti Abhishek <valetiabhishek@Valetis-MacBook-Air.local>
2026-03-13 15:53:13 +0530

    updated backend

--- src/main/java/com/example/InternshipPortal/Controller/UserController.java
+++ src/main/java/com/example/InternshipPortal/Controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://smart-internship-portal-frontend.onrender.com")
 @RequestMapping("/api")
 public class UserController {
 
